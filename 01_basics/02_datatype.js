ea7cdd083f6fff2840a2fc65c9e6fbee98e93aaf
vandanabhay1997@gmail.com <vandanabhay1997@gmail.com>
2025-06-05 23:45:28 +0530

    my day_2 works

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version (no need to write "use strict" in every file)
+
+// alert("Hello World"); // This will show an alert box with the message "Hello World" (not recommended in modern web development as we are using node js instead of browser)
+
+// console.log(3 + 3); // This will print the result of 3+3 to the console
+// code readability is very important in programming, so use console.log to print the output to the console instead of alert
+
+let firstName = "Tanishq"; // String data type
+let age = 20; // Number data type
+let isLoggedIn = false; // Boolean data type
+
+
+// number => 2 to power 53
+// String => "Tanishq"
+// BigInt => 1234567890123456789012345678901234567890n (for very large integers)
+// Boolean => true or false
+// null => empty value (no value assigned)
+// undefined => variable is declared but not assigned a value
+// Symbol => unique and immutable value (used for object properties)
+// Object => collection of key-value pairs (like a dictionary)
+console.log(typeof(firstName)); // This will print the data type of firstName variable
+// null is a object in javascript
