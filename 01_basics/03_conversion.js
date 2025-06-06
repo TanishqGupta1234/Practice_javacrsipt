15f3ba69e6a828335c09ac50747b1f0b3732f528
vandanabhay1997@gmail.com <vandanabhay1997@gmail.com>
2025-06-07 00:10:34 +0530

    my works

--- 01_basics/03_conversion.js
+++ 01_basics/03_conversion.js
@@ -1,25 +1,56 @@
-let score = "tanishq";
-console.log(typeof(score)); // type of score is number
-console.log(typeof score);
+// let score = "tanishq";
+// console.log(typeof(score)); // type of score is number
+// console.log(typeof score);
 
 
 
-// let valueInNumber = Number(score)
-// console.log(typeof valueInNumber); // type of valueInNumber is number
-// console.log(valueInNumber); // 25
+// // let valueInNumber = Number(score)
+// // console.log(typeof valueInNumber); // type of valueInNumber is number
+// // console.log(valueInNumber); // 25
 
-//"33" => 33
-//"33abc" => NaN
-//true => 1 false => 0
+// //"33" => 33
+// //"33abc" => NaN
+// //true => 1 false => 0
 
-let isLoggedIn = "tanishq";
-let isLoggedInNumber = Boolean(isLoggedIn);
-console.log(isLoggedInNumber); // type of isLoggedInNumber is boolean
+// let isLoggedIn = "tanishq";
+// let isLoggedInNumber = Boolean(isLoggedIn);
+// console.log(isLoggedInNumber); // type of isLoggedInNumber is boolean
 
-// 1=> true 0 => false
-// NaN => false
-//"tanishq" => true
+// // 1=> true 0 => false
+// // NaN => false
+// //"tanishq" => true
 
-let someNumber = 33;
-let stringNumber = String(someNumber);
-console.log(typeof stringNumber); // type of stringNumber is string
+// let someNumber = 33;
+// let stringNumber = String(someNumber);
+// console.log(typeof stringNumber); // type of stringNumber is string
+
+
+// ************* operations ****************
+
+let value = 25;
+let negval = -value;
+// console.log(negval); // -25
+
+console.log();
+
+let str1 = "hello";
+let str2 = "world";
+
+let str3 = str1 + str2; // concatenation
+
+// console.log(str3); // helloworld
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 2 + 3); // 12
+// console.log(1 + 2 + "3"); // 33
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter); // 101
---
+++ 01_basics/04_comparsion.js
@@ -0,0 +1,14 @@
+// console.log(2>1); // true
+
+// console.log("2" > 1); // true
+// console.log(1 < "02"); // true
+// console.log(null ==0);
+// console.log(null >=0); // true
+
+console.log(undefined == 0); // true
+console.log(undefined < 0); // false
+console.log(undefined > 0); // false
+
+console.log("2" === 2); // false
+
+// No need to do these type of comparisons
