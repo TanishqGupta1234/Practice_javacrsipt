9451bd55bc04b5afd5ac7f5b3e33fd19ed96a9ee
vandanabhay1997@gmail.com <vandanabhay1997@gmail.com>
2025-06-06 23:33:52 +0530

    my work of day 3

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,25 @@
+let score = "tanishq";
+console.log(typeof(score)); // type of score is number
+console.log(typeof score);
+
+
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber); // type of valueInNumber is number
+// console.log(valueInNumber); // 25
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1 false => 0
+
+let isLoggedIn = "tanishq";
+let isLoggedInNumber = Boolean(isLoggedIn);
+console.log(isLoggedInNumber); // type of isLoggedInNumber is boolean
+
+// 1=> true 0 => false
+// NaN => false
+//"tanishq" => true
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(typeof stringNumber); // type of stringNumber is string
