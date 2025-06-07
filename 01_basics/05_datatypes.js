34a4e6f17e2b9086d0415c8bba5aa772a04e960a
vandanabhay1997@gmail.com <vandanabhay1997@gmail.com>
2025-06-07 23:23:48 +0530

    my datatypes

---
+++ 01_basics/05_datatypes.js
@@ -0,0 +1,52 @@
+// primitve data types
+// string, number, boolean, null, undefined, symbol, bigint
+// string 
+// number
+// null
+//undefined
+// boolean
+// symbol
+// bigint
+
+
+
+const score = 100; // number
+
+const isLoggedIn = true; // boolean
+const outsideTemp = null; // null
+let userEmail; // undefined
+const id = Symbol('id'); // symbol
+
+const bigNumber = 1234567890123456789012345678901234567890n; // bigint
+
+//refrence (non-primitive) data types
+// Array, objects and functions
+
+const heros = ['Thor', 'Ironman', 'Hulk']; // Array
+let myobj = {
+    name:"tanishq",
+    age:22,
+    isActive:true
+
+}
+
+function myFunc() {
+    console.log("Hello from myFunc");
+} // function
+
+console.log(typeof score); // number
+
+// Primitive Data Types:
+
+// Number -> Example: 42
+// String -> Example: "Hello, World"
+// Boolean -> Example: true
+// null -> Example: null
+// undefined -> Example: undefined
+// Symbol -> Example: Symbol("unique")
+// Non-Primitive Data Types:
+
+// Array -> Example: var fruits = ['apple', 'banana', 'cherry']
+// Function -> Example:
+// function greet(name) {
+//   console.log("Hello, " + name + "!");
