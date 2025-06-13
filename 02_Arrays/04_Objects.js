e876fab85df58617834658c7aa5ac95d0d9015d2
vandanabhay1997@gmail.com <vandanabhay1997@gmail.com>
2025-06-13 10:52:54 +0530

    my work

---
+++ 02_Arrays/04_Objects.js
@@ -0,0 +1,52 @@
+// const tinderUser = new Object();
+const tinderUser = {}
+
+tinderUser.id = "123abs";
+tinderUser.name = "John";
+tinderUser.isLoggedIn = false; 
+
+
+// console.log(tinderUser);
+
+
+const regularUser ={
+    email : "some@gamil.com",
+    fullName: {
+        userfullname: {
+            firstname : "John",
+            lastname : "Doe"
+        }
+    }
+}
+
+
+// console.log(regularUser.fullName?.userfullname.firstname);
+
+const obj1 = {
+    1: "one",
+    2: "two",
+}
+
+const obj2 ={
+    3 : "three",
+    4: "four",
+}
+
+// const obj3 = Object.assign({},obj1, obj2);
+const obj3 = {...obj1, ...obj2};
+// console.log(obj3);
+
+const user = [
+    {
+        name : "John",
+        age: 30,
+        email : "tanshs@gmail.com"
+    
+    }
+]
+
+// user[1].email // console.log(user[1]?.email); // undefined, no error
+console.log(Object.keys(user)); // ['name', 'age', 'email']
+console.log(Object.values(user)); 
+console.log(Object.entries(user)); 
+console.log(tinderUser.hasOwnProperty("name")); // true
