ae70b4569a213fc487e7b05e0643640c8bdf7368
vandanabhay1997@gmail.com <vandanabhay1997@gmail.com>
2025-06-15 23:31:10 +0530

    my works

--- 03_basics.js/02_Scopes.js
+++ 03_basics.js/02_Scopes.js
@@ -12,4 +12,52 @@ var c = 30;
     
 }
 
-console.log(`a: ${a},  c: ${c}`); // a: 10, b: 20, c: 30
+// console.log(`a: ${a},  c: ${c}`); // a: 10, b: 20, c: 30
+
+
+function one()
+{
+    const username = "John";
+
+    function two()
+    {
+        const password = "1234";
+        console.log(`Username: ${username}, Password: ${password}`);
+    }
+    // console.log(password);
+    two()
+}
+
+// one();
+
+if (true) {
+ 
+    const username = "John";
+    if (username=="John") {
+        const Website = " youtube"
+        // console.log(username + Website);
+
+    }
+    // console.log(Website); // ReferenceError: Website is not defined
+}
+
+// console.log(username); // ReferenceError: username is not defined
+
+
+
+// +++++++++++++++++++++ Intresting +++++++++++++++++++++++
+
+function addone(num)
+{
+   return num + 1;
+}
+
+addone(10); // 11
+
+
+
+const addTwo = function(num){
+    return num + 2;
+}
+
+addTwo(10); // 12
