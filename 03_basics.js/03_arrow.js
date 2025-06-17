d6bcea85c54a1600ac764c9fb6af017b857115c4
vandanabhay1997@gmail.com <vandanabhay1997@gmail.com>
2025-06-17 23:37:32 +0530

    my work

---
+++ 03_basics.js/03_arrow.js
@@ -0,0 +1,45 @@
+const user ={
+    username: "John",
+    price : 100,
+
+    welcomeMessage: function() {
+        console.log(`Welcome ${this.username}, your price is ${this.price}`); 
+        console.log(this);
+    }
+
+    // this is used to refer to the current context of the object
+}
+
+// user.welcomeMessage(); // Welcome John, your price is 100
+// user.username = "Doe";
+// user.welcomeMessage()
+
+
+// console.log(this);
+
+function one()
+{
+    // let username = "John";
+    // console.log(this.username); // undefined in Node.js, "John" in browser
+}
+
+// one(); // Window object in browser, global object in Node.js
+
+// const chai = () => {
+//     let username = "John";
+//     console.log(this.username); 
+
+// }
+// chai(); // undefined in Node.js, "John" in browser
+
+// const addTwo = (num1, num2) =>{ // Arrow function syntax it is a basic arrow function
+//     return num1 + num2;
+
+// }
+
+// const addTwo = (num1, num2) => num1 + num2; // Arrow function syntax it is a basic arrow function
+// const addTwo2 = (num1, num2) => (num1 + num2); // Arrow function syntax it is a basic arrow function but no need to use retrun keywords
+const addTwo = (num1, num2) => ({username : "John"}); // Arrow function syntax it is a basic arrow function but no need to use retrun keywords   
+
+
+console.log(addTwo(10, 20)) // 30);
