f824094183f6da3eba4ae3f74380e5406c367db7
vandanabhay1997@gmail.com <vandanabhay1997@gmail.com>
2025-06-19 20:54:27 +0530

    my loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,29 @@
+//while
+// let index = 0;
+
+// while (index <= 10) {
+//     console.log(index);
+//     index = index + 2;
+    
+// }
+
+let myArray = ["flash", "batman", "superman"]
+let arr = 0;
+
+while(arr< myArray.length) {
+    // console.log(myArray[arr]);
+    arr++;
+}
+
+// do while
+let socre = 11;
+do {
+    console.log(`Your score is ${socre}`);
+    socre++;
+    
+} while (socre < 5);
+
+// condtiton will be checked after the first iteration 
+// so it will run at least once
+
+
