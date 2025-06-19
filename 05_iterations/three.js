dc99c71dd3229e9ff53bea89b1e84b5e9d47016f
vandanabhay1997@gmail.com <vandanabhay1997@gmail.com>
2025-06-19 21:04:02 +0530

    my arrays loop

---
+++ 05_iterations/three.js
@@ -0,0 +1,12 @@
+// for of loop
+
+const numbers = [1, 2, 3, 4, 5];
+for (const i of numbers) {
+  // console.log(i);
+}
+
+const greetings = ["Hello", "Hi", "Hey"];
+
+for (const greeting of greetings) {
+  // console.log(greeting);
+}
