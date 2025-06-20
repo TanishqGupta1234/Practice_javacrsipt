45b1375f4a45a7707eb2107d55ae4a8711203d77
vandanabhay1997@gmail.com <vandanabhay1997@gmail.com>
2025-06-20 10:31:10 +0530

    my arrays loops

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -10,3 +10,26 @@ const greetings = ["Hello", "Hi", "Hey"];
 for (const greeting of greetings) {
   // console.log(greeting);
 }
+
+// Maps 
+
+const map = new Map()
+map.set("name", "John");
+map.set("age", 30);
+map.set("city", "New York");
+// console.log(map);
+
+for (const [key,value] of map) {
+     console.log(key, value);   
+}
+
+const myObject = {
+    'name': 'Alice',
+    'age': 25,
+    'city': 'Los Angeles'
+}
+
+for (const [key,value] of myObject) {
+    
+    
+}
