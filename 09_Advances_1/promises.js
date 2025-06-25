66c1a47ef684632eaed5895ba5e239bd77c41fbd
vandanabhay1997@gmail.com <vandanabhay1997@gmail.com>
2025-06-25 23:39:44 +0530

    my works

---
+++ 09_Advances_1/promises.js
@@ -0,0 +1,5 @@
+// promises are used to handle asynchronous operations in JavaScript.
+// defining a promise - a promise is an object that represents the eventual completion (or failure) of an asynchronous operation and its resulting value.
+// example of a promise are fetching data from an API, reading a file, etc.
+// a promise is one of these states - pending, fulfilled, or rejected.
+
