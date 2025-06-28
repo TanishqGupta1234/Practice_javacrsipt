106dcbf02f6a666880d4180ee3bb03295614b134
vandanabhay1997@gmail.com <vandanabhay1997@gmail.com>
2025-06-28 20:26:52 +0530

    my calls

--- 10_classes/call.js
+++ 10_classes/call.js
@@ -1,13 +1,18 @@
 function SetUserName(username)
 {
-    this.username = username
+    this.username = username;
+    // console.log("called");
 }
 
 function createUser(username, email, password){
-    SetUserName.call(this, username) // Using call to set the username
+    SetUserName.call(this,username) // Using call to set the username
     this.email = email
     this.password = password
 }
 
 const chai = new createUser("chai", "tanishq@12.com", "123")
 console.log(chai);
+
+//call() is a method in JavaScript that lets you manually set the value of this when calling a function.
+// In short:
+// "Call kisi function ko kisi aur object ke sath chalane ka tareeka hai."
